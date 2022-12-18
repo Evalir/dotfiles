b3281815977b60f7a18a25b5664fa3de984bc383
Enrique Ortiz <hi@enriqueortiz.dev>
2022-12-17 21:58:45 -0400

    chore: stop trying to make the bg transparent, kinda sucks

--- nvim/lua/evalir/macOS/colors.lua
+++ nvim/lua/evalir/macOS/colors.lua
@@ -1,8 +1,6 @@
 function ColorBg(color)
   color = color or "github_dark_default"
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorBg()
